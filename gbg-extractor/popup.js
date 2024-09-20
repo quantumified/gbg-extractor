c7c1628ee1b984399edf28984abbd498fd590201
quantumified <quantumified@gmail.com>
2024-09-20 17:28:50 +1000

    Update popup.js

--- gbg-extractor/popup.js
+++ gbg-extractor/popup.js
@@ -1,24 +1,3 @@
-document.getElementById('extractBtn').addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.scripting.executeScript({
-            target: { tabId: tabs[0].id },
-            func: extractData
-        }).catch(error => {
-            console.error('Error:', error);
-            document.getElementById('error').innerText = 'Failed to execute script: ' + error.message;
-        });
-    });
-    
-    document.getElementById('status').innerText = "Data extraction triggered.";
+document.getElementById('send').addEventListener('click', () => {
+    chrome.runtime.sendMessage({ action: 'sendMessage' });
 });
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === 'ERROR') {
-        document.getElementById('error').innerText = message.errorMessage;
-    }
-});
-
-function extractData() {
-    // This function will be executed in the context of the web page
-    chrome.runtime.sendMessage({ type: 'EXTRACT_GBG_DATA' });
-}
