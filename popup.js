f5b90a02d1a679f3821e7a954115a4bab7c508b4
quantumified <quantumified@gmail.com>
2024-10-23 09:59:46 +1100

    Delete popup.js

--- popup.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("logMapButton").addEventListener("click", function() {
-  console.log('Log Map Data button clicked');
-  chrome.runtime.sendMessage({ action: "logMapData" });
-});
