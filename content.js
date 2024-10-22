3f56a4d1bdbd5dc0961f32a7f9570857e894f966
quantumified <quantumified@gmail.com>
2024-10-23 09:55:14 +1100

    Delete content.js

--- content.js
+++
@@ -1,9 +0,0 @@
-// Ensure FoEproxy is loaded
-if (typeof FoEproxy !== 'undefined') {
-  FoEproxy.addHandler('GuildBattlegroundService', 'getBattleground', (data, postData) => {
-      const mapData = data.responseData.map.provinces;
-      console.log('Full map data:', mapData);
-  });
-} else {
-  console.error('FoEproxy is not available.');
-}
